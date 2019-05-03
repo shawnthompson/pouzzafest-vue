078b53ac81299dacf5498c4736fc01ff35d32ed2
Shawn Thompson <plansmash@me.com>
2019-05-02 21:35:45 -0400

    add data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Pouzzafest 2019</h1>
+    <ul>
+    <li v-for="band in bands" :key="band.id">
+      {{ band }}
+    </li>
+    </ul>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
+import data from "../data/data.json";
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // },
+  computed: {
+    bands() {
+      return data.bands.map((item) => {
+        return item.bandName;
+      })
+    }
   }
 };
 </script>
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
